593dbed3c25730d78432285f835405ccb3be7d74
tsh96 <tansiahow1996@gmail.com>
2020-06-06 22:23:15 +0800

    add google-map to index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,50 +1,31 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    <gmap-map
+      :center="center"
+      :zoom="17"
+      style="width: 100vw; height: calc(100vh - 50px)"
+    >
+      <GmapMarker :position="center" />
+    </gmap-map>
   </q-page>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-import ExampleComponent from 'components/CompositionComponent.vue'
-import { Todo, Meta } from 'components/models'
-
 export default Vue.extend({
   name: 'PageIndex',
-  components: { ExampleComponent },
+  mounted () {
+    navigator.geolocation.watchPosition(position => {
+      this.center.lat = position.coords.latitude
+      this.center.lng = position.coords.longitude
+      console.log(position)
+    })
+  },
   data () {
-    const todos: Todo[] = [
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]
-    const meta: Meta = {
-      totalCount: 1200
+    return {
+      center: { lat: 10, lng: 10 }
     }
-    return { todos, meta }
   }
 })
 </script>
