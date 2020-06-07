4f3720b9d967f1c47dc0dc9991ebc7fb3e6f9889
tsh96 <tansiahow1996@gmail.com>
2020-06-07 14:38:32 +0800

    change icon of self location add "Add Target Location" button add markers for target locations

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,12 +6,28 @@
       :zoom="17"
       style="width: 100vw; height: calc(100vh - 50px)"
     >
-      <GmapMarker :position="center" />
+      <GmapMarker
+        :position="center"
+        :icon="icon"
+      />
+      <GmapMarker
+        v-for="(position, index) in targetLocations"
+        :key="index"
+        draggable
+        @position_changed="targetLocationChangedHandler(index, $event)"
+        :position.sync="position"
+      />
       <gmap-circle
         :center="center"
         :radius="accuracy"
       />
     </gmap-map>
+    <q-btn
+      label="Add Target Location"
+      style="position: absolute; bottom: 8px"
+      @click="addTargetLocation()"
+      color="primary"
+    />
   </q-page>
 </template>
 
@@ -34,7 +50,9 @@ export default Vue.extend({
   data () {
     return {
       center: { lat: 10, lng: 10 },
+      targetLocations: [] as { lat: number; lng: number }[],
       accuracy: 0,
+      icon: { path: 'M 0 5 a 5 5 0 1 1 10 0 a 5 5 0 1 1 -10 0 Z', anchor: { x: 5, y: 5 } },
       mapOptions: {
         zoomControl: true,
         mapTypeControl: false,
@@ -44,6 +62,18 @@ export default Vue.extend({
         fullscreenControl: true,
         disableDefaultUI: false
       }
+    }
+  },
+  methods: {
+    addTargetLocation () {
+      this.targetLocations.push({
+        lat: this.center.lat,
+        lng: this.center.lng
+      })
+    },
+    targetLocationChangedHandler (index: number, val: { lat: () => number; lng: () => number }) {
+      this.targetLocations[index].lat = val.lat()
+      this.targetLocations[index].lng = val.lng()
     }
   }
 })
